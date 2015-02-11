5471efb48154befdd26c2d4d37e8a46f09cea2e2
HeroCC <HeroCC@users.noreply.github.com>
2015-02-11 14:09:44 -0500

    Added more MOTD modes

--- zsh/motd.zsh
+++ zsh/motd.zsh
@@ -1,10 +1,22 @@
 if [[ -n $SSH_CONNECTION ]]; then
+	motd_mode=none
+fi
 
-else
-	if [ "$MOTD_MODE" = norris ]; then
-		norris 2
-	elif
-		[ "$MOTD_MODE" = screenfetch ]; then
-		screenfetch
-	fi
+if [ "$motd_mode" = norris ]; then
+	norris 2
+elif
+	[ "$motd_mode" = screenfetch ]; then
+	screenfetch
+elif
+	[ "$motd_mode" = fortune ]; then
+	fortune | cowsay -f $cow
+elif
+	[ "$motd_mode" = fortune_nocow ]; then
+	fortune
+elif
+	[ "$motd_mode" = none ]; then
+		#Nothing
+elif 
+	[ "$motd_mode" ]; then
+	echo "Unknown motd_mode '$motd_mode', please check your .zshrc or motd file"
 fi
