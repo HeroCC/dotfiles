2f91625c4e2999724ed5c77201bd50dddbcc6394
HeroCC <HeroCC@users.noreply.github.com>
2015-02-19 20:27:52 -0500

    Added sources to a few aliases, and made rm an alias of trash-put

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,13 +9,14 @@ alias ri='ri -Tf ansi'
 alias mysql='mysql -u root'
 alias mysqladmin='mysqladmin -u root'
 
-alias sudo='sudo ' #Makes sudo use aliases
-alias apt-get='apt-fast'
+alias sudo='sudo ' # Makes sudo use aliases, from http://go.herocc.com/sudo_aliases
+alias apt-get='apt-fast' # http://go.herocc.com/aptfast
 alias gpg='gpg2'
 alias please='sudo $(fc -nl -1)'
+alias cls='clr'
 
-#I like verbose in chmod & chown
+# I like verbose in chmod & chown
 alias chmod='chmod -v'
 alias chown='chown -v'
 
-alias cls="clr"
+alias rm='trash-put' # Requires trash-cli
