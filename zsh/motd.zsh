ef76c02b829500b0d12c06f4cea13b3b06dcf8ed
HeroCC <HeroCC@users.noreply.github.com>
2015-02-19 20:28:34 -0500

    Added a new MOTD mode: updates. Lists avalible updates

--- zsh/motd.zsh
+++ zsh/motd.zsh
@@ -14,8 +14,15 @@ elif
 	[ "$motd_mode" = fortune_nocow ]; then
 	fortune
 elif
+	[ "$motd_mode" = updates ]; then
+	local updates=$(/usr/lib/update-notifier/apt-check 2>&1)
+	local sec_updates=$(echo "$updates" | cut -d ";" -f 2)
+	local non_sec_updates=$(echo "$updates" | cut -d ";" -f 1)
+
+	echo "There are $sec_updates security updates and $non_sec_updates regular updates"
+elif
 	[ "$motd_mode" = none ]; then
-		#Nothing
+	clear
 elif 
 	[ "$motd_mode" ]; then
 	echo "Unknown motd_mode '$motd_mode', please check your .zshrc or motd file"
