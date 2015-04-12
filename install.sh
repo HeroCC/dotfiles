42c8dee8d6a71f0bcd0e437ad617cde9fb03f9fe
HeroCC <HeroCC@users.noreply.github.com>
2015-04-12 12:46:57 -0400

    Change all paths to use $DOTFILE_DIR

--- install.sh
+++ install.sh
@@ -23,9 +23,9 @@ function installConfigs {
   #ZSH
   update zsh
   echo "Installing ZSH Config"
-  ln -s ~/.dotfiles/zsh ~/.zsh
-  ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
-  ln -s ~/.dotfiles/zsh/zshenv ~/.zshenv
+  ln -s $DOTFILE_DIR/zsh ~/.zsh
+  ln -s $DOTFILE_DIR/zsh/zshrc ~/.zshrc
+  ln -s $DOTFILE_DIR/zsh/zshenv ~/.zshenv
   git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   if [ "$CI" == "false" ]; then
     chsh -s $(which zsh)
@@ -34,25 +34,25 @@ function installConfigs {
   #git
   update git
   echo "Installing Git Config"
-  ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-  ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+  ln -s $DOTFILE_DIR/git/gitconfig ~/.gitconfig
+  ln -s $DOTFILE_DIR/git/gitignore_global ~/.gitignore_global
 
   #Screen
   update screen
   echo "Installing Screen Config"
-  ln -s ~/.dotfiles/screen/screenrc ~/.screenrc
+  ln -s $DOTFILE_DIR/screen/screenrc ~/.screenrc
 
   #SSH
   update openssh-client
   update openssh-server
   echo "Installing SSH Config"
-  mkdir ~/.dotfiles/ssh/
-  ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+  mkdir $DOTFILE_DIR/ssh/
+  ln -s $DOTFILE_DIR/ssh/config ~/.ssh/config
 
   #Gem
   update ruby-full
   echo "Installing Gem Config"
-  ln -s ~/.dotfiles/gem/gemrc ~/.gemrc
+  ln -s $DOTFILE_DIR/gem/gemrc ~/.gemrc
   echo Installing Rbenv
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
@@ -60,21 +60,21 @@ function installConfigs {
   echo "Installing Sublime Text 3 Config"
   mkdir ~/.config/sublime-text-3/Packages/
   cd ~/.config/sublime-text-3/Packages/
-  ln -s ~/.dotfiles/sublimetext/User User
+  ln -s $DOTFILE_DIR/sublimetext/User User
 
   #Terminator
   echo "Installing Terminator Config"
   mkdir ~/.config/terminator/config
-  ln -s ~/.dotfiles/terminator/config ~/.config/terminator/config
+  ln -s $DOTFILE_DIR/terminator/config ~/.config/terminator/config
   update terminator
 
 
   #VIM
   update vim
   echo "Installing VIM config"
-  ln -s ~/.dotfiles/vim ~/.vim
-  ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-  ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
+  ln -s $DOTFILE_DIR/vim ~/.vim
+  ln -s $DOTFILE_DIR/vim/vimrc ~/.vimrc
+  ln -s $DOTFILE_DIR/vim/gvimrc ~/.gvimrc
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 }
@@ -93,7 +93,7 @@ done
 
 if [ "$USER" == "travis" ]; then
   CI='true'
-  DOTFILE_DIR="HeroCC/dotfiles"
+  DOTFILE_DIR="$(pwd)"
 fi
 
 if [ "$noInstall" == 'true' ]; then
