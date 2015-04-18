b407987d3c52835e70f48293da2a52af188caff5
HeroCC <HeroCC@users.noreply.github.com>
2015-04-18 11:49:56 -0400

    No more norris.zsh, moved to functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -104,3 +104,14 @@ function update_gitpulls {
   fi
   cd $current_dir
 }
+
+function norris {
+  if [ "$PS1" ]; then
+    if [ "$1" ]; then
+      timeout $1 wget "http://api.icndb.com/jokes/random" -qO- | jshon -e value -e joke -u |
+      recode html | cowsay -f tux
+    else
+      norris 3
+    fi
+  fi
+}
