2ee75c0d7e5e12200f246fa40f660b54e86d1d68
HeroCC <HeroCC@users.noreply.github.com>
2015-06-22 14:48:42 -0400

    Add alert command from default Ubuntu .bashrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -44,3 +44,5 @@ alias mkdir='mkdir -pv'
 ialias git='hub'
 
 alias jenv="_jenv"
+
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
