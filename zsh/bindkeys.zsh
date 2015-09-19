cc6999271d000f9e4a1f3cda18e3f2b810b0734a
HeroCC <HeroCC@herocc.com>
2015-09-19 17:17:19 -0400

    Use VIM zsh keybindings

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -7,11 +7,4 @@ bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
 
-#OHMYZSH Handles that for us
-#bindkey "[B"      history-search-forward               # down arrow
-#bindkey "[A"      history-search-backward              # up arrow
-
-bindkey "^D"      delete-char                          # ctrl-d
-bindkey "^F"      forward-char                         # ctrl-f
-bindkey "^B"      backward-char                        # ctrl-b
-bindkey -e   # Default to standard emacs bindings, regardless of editor string
+bindkey -v   # Default to standard vim bindings, regardless of editor string
