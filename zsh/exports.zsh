341ad3a40f078c58dfcb3caf6358c06cc3f7a1bb
HeroCC <HeroCC@herocc.com>
2015-09-25 14:05:38 -0400

    Commented LS_COLORS var for now

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
 export CLICOLOR=1
-export LSCOLORS=Gxfxcxdxbxegedabagacad
+#export LS_COLORS=Gxfxcxdxbxegedabagacad
 
 # Enable color in grep
 export GREP_OPTIONS='--color=auto'
