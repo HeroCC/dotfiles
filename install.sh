a43921cd3752645e1da030c712c4f9c5de796f74
HeroCC <HeroCC@herocc.com>
2015-10-01 12:08:36 -0400

    Fixed Submodules again

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 function pull {
   cd $DOTFILE_DIR
   git pull origin master
-  git submodule foreach git submodule update --init
+  git submodule init
+  git submodule update
+  # git submodule foreach git submodule update --init
   # git submodule foreach update --init
 }
 
