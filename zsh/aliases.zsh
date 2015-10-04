63e659a2c222dd19317c4eeca624dca0bef181f5
HeroCC <HeroCC@herocc.com>
2015-10-04 14:26:32 -0400

    Part 2 of loosing it

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,6 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
 alias ls='ls -GFh'
 
-# Same as above, but in long listing format
-alias ll='ls -GFhl'
-
 alias mysql='mysql -u root'
 alias mysqladmin='mysqladmin -u root'
 
