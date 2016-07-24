25106c1001e53a1cbe500505ba445de702c42a58
HeroCC <herocc@herocc.com>
2016-07-24 19:15:23 -0400

    Add a server setup script

---
+++ serverSetup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "HeroCC's Ubuntu (14.04) Server Setup script!"
+echo "This is aimed at the Ubuntu 14.04 Proxmox template"
+packagesToInstall="curl git vim unattended-upgrades"
+
+function main {
+  ## INSTALL REGULAR PACKAGES
+  echo "Installing: $packagesToInstall"
+  sudo apt-get install $packagesToInstall
+
+  ## Setting Timezone
+  echo "Setting Timezone to America/New_York (EST) with DST"
+  echo "America/New_York" | sudo tee /etc/timezone
+
+  ## Generating en_US and en_US.UTF8 localization
+  echo "Generating en_US and en_US.UTF-8 localizations"
+  sudo locale-gen en_US en_US.UTF-8
+  sudo dpkg-reconfigure locales
+  echo "Supported Locales: $(cat /var/lib/locales/supported.d/local)"
+
+  ## Configure Unattended Upgrades
+  echo "Please select YES to automatically install security packages"
+  sudo dpkg-reconfigure unattended-upgrades
+
+  ## Prompting for dotfiles install
+  echo "Do you want to install personal dotfiles? [Y/n]"
+  read installDotfiles
+  if [ "$installDotfiles" = y ] || [ "$installDotfiles" = Y ] || [ -z "$installDotfiles" ]; then
+    git clone https://github.com/HeroCC/dotfiles.git ~/.dotfiles && ~/.dotfiles/install.sh
+    echo "Dotfiles Install finished"
+  fi
+}
+
+main "$@"
