98cb7d13cd5f0d89933623cf72174bb354ddf6c7
HeroCC <herocc@herocc.com>
2019-07-21 19:39:43 -0400

    Remove unneeded command escaping

--- zsh/preload.zsh
+++ zsh/preload.zsh
@@ -13,5 +13,5 @@ function exitt {
 
 if [[ -z "$TMUX_AUTOSTARTED" && -z "$SSH_CONNECTION" ]]; then
   export TMUX_AUTOSTARTED='true'
-  \tmux -2 attach -t m || \tmux -2 new-session -s m && trytmuxexit
+  tmux -2 attach -t m || tmux -2 new-session -s m && trytmuxexit
 fi
