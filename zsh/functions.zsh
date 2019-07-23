f0bc7076d9da33d0bd69663834790c081a5b54fa
HeroCC <herocc@herocc.com>
2019-07-22 20:58:56 -0400

    Swap to plugins for Magic-Enter and tmux autostart

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,18 +1,3 @@
-# Detect empty enter, execute git status if in git dir
-magic-enter () {
-  if [[ -z $BUFFER ]]; then
-    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-      echo -ne '\n'
-      git status
-    fi
-    zle accept-line
-  else
-    zle accept-line
-  fi
-}
-zle -N magic-enter
-bindkey "^M" magic-enter
-
 function bgsc { # Start screen session in background if it doesn't exist
 if screen -list | awk '{print $1}' | grep -q "$1$"; then
   echo "screen $1 already exists"
