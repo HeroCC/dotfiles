b8ccfedb79f3b95d0981c1e924743c9208af5f3c
Conlan Cesar <conlanc@gmail.com>
2020-04-08 18:17:09 -0400

    Colorize `ls` on mack and linux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,10 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFph --color=auto'
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  colorflag="--color"
+else # OS X `ls`
+  colorflag="-G"
+fi
+alias ls="ls -Fph ${colorflag}"
 
 alias sudo='sudo ' # Makes sudo use aliases, from https://askubuntu.com/a/22043/353466
 alias please='sudo $(fc -nl -1)' # Repeat last command with sudo
