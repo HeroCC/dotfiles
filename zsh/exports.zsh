49908ac335d9db36563783e6229cc340c6adbbe2
Conlan Cesar <conlanc@gmail.com>
2023-08-15 22:40:15 -0400

    Remove old workaround for benign XCode bug
    Seems to be fixed in newer releases.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -12,8 +12,5 @@ export LESS='--ignore-case --raw-control-chars'
 
 export PATH="$HOME/.dotfiles/bin:$PATH"
 
-# https://stackoverflow.com/a/55344565/1709894
-export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
-
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
 
