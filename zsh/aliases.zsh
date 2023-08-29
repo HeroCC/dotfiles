c575ebdd6515e6d7bff80e9ff921bf021de19de2
Conlan Cesar <conlan.cesar@datadoghq.com>
2023-08-29 16:31:25 -0400

    zsh: simplify ls colorization
    Looks like MacOS `ls` supports the `--color` flag now, so remove the check.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,5 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-  colorflag="--color"
-else # OS X `ls`
-  colorflag="-G"
-fi
-alias ls="ls -Fph ${colorflag}"
+alias ls="ls --color -Fh"
 
 alias sudo='sudo ' # Makes sudo use aliases, from https://askubuntu.com/a/22043/353466
 alias please='sudo $(fc -nl -1)' # Repeat last command with sudo
