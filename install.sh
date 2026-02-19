bad9858f49961e485433effd3c7df18f4ff66677
Conlan Cesar <conlan.cesar@datadoghq.com>
2026-02-19 17:04:53 -0500

    More bootstrapable install

--- install.sh
+++ install.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-function pull {
-  cd $DOTFILE_DIR
-  git pull origin master
-  git submodule init
-  git submodule update
-  # git submodule foreach git submodule update --init
-  # git submodule foreach update --init
-}
-
 function runAnsible {
-    cd $DOTFILE_DIR
     ./run-ansible.sh
 }
 
 function main {
-    DOTFILE_DIR="$HOME/.dotfiles"
-    [ "$USER" == "travis" ] && DOTFILE_DIR="$(pwd)"
-    pull
+    [ "$USER" == "bits" ] && sudo apt-get install ansible
     runAnsible
 }
 
