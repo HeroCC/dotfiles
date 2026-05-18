be723f84b6cb6589ba857954080f10d8c6a28349
Conlan Cesar <conlan.cesar@datadoghq.com>
2026-05-18 11:43:26 -0400

    `cd` to file

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -20,3 +20,12 @@ calc="${calc//x/*}"
 bc -l <<<"scale=10;$calc"
 }
 
+cd() {
+  # If given a file path, cd to its parent directory. ${1:h} is zsh's :h modifier
+  # (borrowed from vi), which strips the trailing filename component — like dirname.
+  if [[ $# -eq 1 && -f "$1" ]]; then
+    builtin cd "${1:h}"
+  else
+    builtin cd "$@"
+  fi
+}
