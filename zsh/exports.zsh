8247fc02e5567ccf5ddc624f5ec7ca5212344df9
conlan.cesar <conlan.cesar@datadoghq.com>
2026-08-20 17:06:03 -0400

    add $HOME/bin to path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -27,7 +27,7 @@ fi
 export PAGER='less'
 export LESS='--ignore-case --raw-control-chars'
 
-export PATH="$HOME/.dotfiles/bin:$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.dotfiles/bin:$HOME/.local/bin:$PATH"
 
 [[ -f ~/.config/tabtab/__tabtab.zsh ]] && source ~/.config/tabtab/__tabtab.zsh || true
 
